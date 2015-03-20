6b981235581dc089857a7910360ba363109011a4
eric-wang <starwz@gmail.com>
2015-03-20 17:10:01 +0800

    Update CLog.h

--- cutil/include/CLog.h
+++ cutil/include/CLog.h
@@ -1,9 +1,20 @@
-/* 
- *	CLog.h -- Runtime log header
+/*
+ * Copyright (C) 2015 The CUtil Project
  *
- *	log and analysis runtime information.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * CLog.h, v 1.0 2010/06/06 14:44:50, Eric Wang
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Eric Wang
+ * Update: 2015-03-20
  */
 
 #ifndef __CLOG_H__
